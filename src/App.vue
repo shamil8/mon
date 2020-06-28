336d621014a8d911ee379d94168dbec2003566eb
Shamil Kurbonov <qurbonovshamil@gmail.com>
2020-06-29 00:53:52 +0700

    Added base packages

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    Hello {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      text: 'Something'
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+
+}
+</style>
